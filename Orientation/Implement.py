8dcd733f2723bad23ad08d2c415fcd61ca72dc3c
icarusization <icarusization@126.com>
2016-09-23 22:45:05 +0800

    Improve on orientation field

--- Orientation/Implement.py
+++ Orientation/Implement.py
@@ -1,37 +1,63 @@
 import random
+import Strokes
 import math
-
-length = 612
-width = 496
-
-
-def set_size(im):
-    length, width = im.size()
+import Image, ImageDraw
+import numpy as np
 
 
-def hsv2rgb(h, s, v):
-    h = float(h)
-    s = float(s)
-    v = float(v)
-    h60 = h / 60.0
-    h60f = math.floor(h60)
-    hi = int(h60f) % 6
-    f = h60 - h60f
-    p = v * (1 - s)
-    q = v * (1 - f * s)
-    t = v * (1 - (1 - f) * s)
-    r, g, b = 0, 0, 0
-    if hi == 0: r, g, b = v, t, p
-    elif hi == 1: r, g, b = q, v, p
-    elif hi == 2: r, g, b = p, v, t
-    elif hi == 3: r, g, b = p, q, v
-    elif hi == 4: r, g, b = t, p, v
-    elif hi == 5: r, g, b = v, p, q
-    r, g, b = int(r * 255), int(g * 255), int(b * 255)
-    return r, g, b
+def neighbour(point, dic, l_max, w_max):
+    x, y = point[0], point[1]
+    l = []
+    for i in [x-1, x, x+1]:
+        for j in [y-1, y, y+1]:
+            if 0<=i<l_max and 0<=j<w_max and (i!=x or j!=y):
+                if dic.has_key((i, j)):
+                    l.append((i, j))
+    return l
 
 
-def transfer_data(f, r, dic):
+def direction_sketch(im):
+    (length, width) = im.size
+    m = np.zeros((length, width), np.int)
+    for i in range(length):
+        for j in range(width):
+            color = im.getpixel((i, j))
+            light = color[0]/255.0
+            if light<0.5:
+                m[i][j] = 0
+            else:
+                m[i][j] = 1
+
+    f = {}
+    for i in range(length):
+        for j in range(width):
+            f[(i, j)] = [0, 0.0]
+
+    angle_dic = {(1, 0):0.0, (1, 1):0.78, (0, 1):1.54, (-1, 1):2.35, (-1, 0):3.14,
+                 (-1, -1):3.92, (0, -1):4.71, (1, -1):5.49}
+    for i in range(length):
+        for j in range(width):
+            point = [i, j]
+            if m[i][j] ==1:
+                l = neighbour(point, f, length, width)
+                for p in l:
+                    if m[p[0]][p[1]] == 1:
+                        angle = angle_dic[(p[0]-i, p[1]-j)]
+                        f[(i, j)] = [1, angle]
+                        break
+    return f
+
+
+def random_noise(length, width):
+    r = {}
+    for i in range(length):
+        for j in range(width):
+            r[(i, j)] = random.random() * 3.14
+    return r
+
+
+def transfer_data(f, r, length, width):
+    dic = {}
     for i in range(length):
         for j in range(width):
             l = []
@@ -41,24 +67,24 @@ def transfer_data(f, r, dic):
                 dic[(i, j)] = l                    # For line point it has weight 1.0
             else:
                 l.append(r[(i, j)])
-                l.append(float(0.01))
+                l.append(float(0.1))
                 dic[(i, j)] = l                    # For noise point it has weight 0.0 to be affected by outline
     return dic
 
 
-def iteration(n, dic):
+def iteration(n, dic, length, width):
     for k in range(n):
         for i in range(length):
             for j in range(width):
                 point = dic[(i, j)]
                 if point[1] != 1.0:
-                    point = square_central(dic, i, j)
+                    point = square_central(dic, i, j, length, width)
                     # point[1] = 2*point[1]/(1+point[1])
                     dic[(i, j)] = point
     return dic
 
 
-def square_central(dic, i, j):
+def square_central(dic, i, j, length, width):
     angle_list = []
     weight_list = []
     angle = 0
@@ -86,3 +112,96 @@ def square_central(dic, i, j):
     weight = min((max((0, weight)), 1.0))
 
     return [angle, weight]
+
+
+def pop_points(dic, point, w):
+    if w!=1:
+        w -= 1
+        x = range(point[0]-w, point[0]+w, 1)
+        y = range(point[1]-w, point[1]+w, 1)
+    else:
+        x = point[0]
+        y = point[1]
+
+    for i in x:
+        for j in y:
+            if (i, j) in dic:
+                del dic[(i, j)]
+    return dic
+
+
+def connect_orientation(dic, max_length, w, im):
+    (length, width) = im.size
+    stroke_list = []
+    pix = im.load()
+
+    while len(dic) != 0:
+        key_value = dic.popitem()
+        begin = key_value[0]
+        s_angle = key_value[1][0]
+
+        axis_line = []
+        point, l = begin, 0
+        con = True
+        color = [0, 0, 0]
+
+        while l<max_length and con:
+            con = False
+            next_point = []
+            for ele in neighbour(point, dic, length, width):
+                angle = dic[ele][0]
+                COS = math.cos(angle-s_angle)
+                if abs(COS)>0.707:
+                    con = True
+                    next_point.append([ele, COS])
+
+            max = -1.0
+            for pair in next_point:
+                if pair[1]>max:
+                    point = pair[0]
+                    max = pair[1]
+            axis_line.append(point)
+            l += 1
+
+        # color = [color[0]/l, color[1]/l, color[2]/l]
+        color = [int(random.random() * 255), int(random.random() * 255), int(random.random() * 255)]
+        if l > 10:
+            stroke_list.append([begin, point, color])
+
+        for points in axis_line:
+            dic = pop_points(dic, points, w)
+
+    return stroke_list
+
+
+def show_stroke(stroke_list, im):
+    (length, width) = im.size
+    s = Strokes.Stroke()
+
+    # Here you can revise these parameters according to the introduction in the Strokes Class.
+    s.distort = 0.2
+    s.shake = 0.3
+    s.tapering = 0.5
+    s.ColorVariability = 0.5
+    s.ShadeVariability = 0.5
+
+    draw = ImageDraw.Draw(im)
+    for l in stroke_list:
+        color = l[2]
+        c = Strokes.Color(color[0], color[1], color[2])
+        s.color = c
+        w = 5
+
+        draw.line(((l[0][0], l[0][1]), (l[1][0], l[1][1])), fill = 255)
+        '''
+        points = s.draw_strokes(im, l[0][0], l[0][1], l[1][0], l[1][1], w, s.color)
+
+        for i in range(len(points)):
+            p = points[i]
+            c = p[2]
+            nc = c.get_color()
+            nc = (int(nc[0] * 255), int(nc[1] * 255), int(nc[2] * 255))
+            if 0<=p[0]<length and 0<=p[1]<width:
+                im.putpixel((p[0], p[1]), nc)
+        '''
+    return im
--- Orientation/Main.py
+++ Orientation/Main.py
@@ -1,59 +1,27 @@
-import Image, ImageDraw
+import Image
 import Implement as Ip
-import random
-import math
 import vector as vc
-
+# Data: 16/09/21       Writer: Xu De Wei
 # Give: Orientation field dictionary f,  random noise field dictionary r
 # Data format: f[(i, j)]: [0/1, angle]        r[(i, j)]: angle
-#              dic[(i, j)]: [angle, weight]
+# Data format: dic[(i, j)]: [angle, weight]
 
-im=Image.open('boundary.jpg')
-length = 612
-width = 496
-im1 = Image.new("RGB", (length, width), (255, 255, 255))
-im2 = Image.new("RGB", (length*2+1, width*2+1), (255, 255, 255))
-f = {}
-r = {}
+im=Image.open('retu1.bmp')
+(length, width) = im.size
 
-f = vc.direction_array(im)
+f = Ip.direction_sketch(im)                       # f is the sketch line graph whose format is f[(i, j)]: [0/1, angle]
 
-for i in range(length):
-    for j in range(width):
-        r[(i, j)] = random.random()*3.14
+r = Ip.random_noise(length, width)               # produce the graph random noise
 
-dic = {}
+dic = Ip.transfer_data(f, r, length, width)      # input f, r to get the whole graph orientation field
 
-# First read the data from f, r into dic
-dic = Ip.transfer_data(f, r, dic)
+num = 30                                       # iteration times
+dic = Ip.iteration(num, dic, length, width)
 
-# Get into iterations to converge direction b
-n = 30    # iteration times
-dic = Ip.iteration(n, dic)
+max_length, w = 20, 3                            # the maximum length and width of stroke
+stroke_list = Ip.connect_orientation(dic, max_length, w, im)
 
-draw = ImageDraw.Draw(im2)
-for i in range(length-1):
-    for j in range(width-1):
-        h = dic[(i, j)][0]
-        r, g, b = Ip.hsv2rgb(h/6.28*360, 1.0, 1.0)
-        im1.putpixel((i, j), (r, g, b))
-        newi = int(i*10+5+10.0*math.cos(h))
-        newj = int(j*10+5+10.0*math.sin(h))
-        draw.line(((i*10+5, j*10+5),(newi, newj)),fill=128)
-        if 1 < newi < length-1 and 1 < newj < width-1:
-            if newi > i * 10 + 5:
-                im2.putpixel((newi - 1, newj), (0, 0, 0))
-            else:
-                im2.putpixel((newi + 1, newj), (0, 0, 0))
-            if newj > j * 10 + 5:
-                im2.putpixel((newi, newj - 1), (0, 0, 0))
-            else:
-                im2.putpixel((newi, newj + 1), (0, 0, 0))
+new_image = Image.new("RGB", (length, width), (255, 255, 255))
+new_image = Ip.show_stroke(stroke_list, new_image)
 
-im1.show()
-im2.show()
-
-pass
-
-
-
+new_image.show()
