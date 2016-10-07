7c4dd78bc2bba1a2e3ed0b450c28895a26dcfcec
Wang Xiangyu <shyay1013@sjtu.edu.cn>
2016-10-07 17:50:44 +0800

    Modified Orientation

---
+++ StrokeProcesses/Neighborhood.py
@@ -0,0 +1,13 @@
+import Implement
+
+class Neighborhood:
+	def __init__(self):
+		self.density=None
+        	self.densitymap=None
+
+        def set_density(self,d):
+		self.density=d
+
+	def set_densitymap(self,m):
+		self.densitymap=m
+
