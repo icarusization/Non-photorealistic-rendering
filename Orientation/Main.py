75011469e17eac79c1e5a700c43ed814184528cb
icarusization <icarusization@126.com>
2016-10-07 14:49:19 +0800

    Better process on faraway space
    Better process on faraway space

--- Orientation/Main.py
+++ Orientation/Main.py
@@ -1,12 +1,11 @@
 import Image
 import Implement as Ip
-import vector as vc
 # Data: 16/09/21       Writer: Xu De Wei
 # Give: Orientation field dictionary f,  random noise field dictionary r
 # Data format: f[(i, j)]: [0/1, angle]        r[(i, j)]: angle
 # Data format: dic[(i, j)]: [angle, weight]
 
-im=Image.open('retu1.bmp')
+im=Image.open('redu.bmp')
 (length, width) = im.size
 
 f = Ip.direction_sketch(im)                       # f is the sketch line graph whose format is f[(i, j)]: [0/1, angle]
@@ -14,12 +13,14 @@ f = Ip.direction_sketch(im)                       # f is the sketch line graph w
 r = Ip.random_noise(length, width)               # produce the graph random noise
 
 dic = Ip.transfer_data(f, r, length, width)      # input f, r to get the whole graph orientation field
+eff = {}
 
-num = 30                                       # iteration times
-dic = Ip.iteration(num, dic, length, width)
+num = 50                                        # iteration times
+eff = Ip.effect_map(dic, length, width)
+dic, eff = Ip.iteration(num, dic, eff, length, width)
 
-max_length, w = 20, 3                            # the maximum length and width of stroke
-stroke_list = Ip.connect_orientation(dic, max_length, w, im)
+max_length, w = 8, 2                           # the maximum length and width of stroke
+stroke_list = Ip.connect_orientation(dic, eff, max_length, w, im)
 
 new_image = Image.new("RGB", (length, width), (255, 255, 255))
 new_image = Ip.show_stroke(stroke_list, new_image)
