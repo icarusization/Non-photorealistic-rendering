c464942ee1508f15e8fe458c336671e48633483b
5133709162 <zl007700@163.com>
2016-10-08 04:09:52 -0700

    give some sample

--- StrokeProcesses/densityMap/density_map.py
+++ StrokeProcesses/densityMap/density_map.py
@@ -1,13 +1,13 @@
 import numpy as np
 import cv2
 import math as m
-
+import sys
 
 def get_hist(image):  
     if len(image.shape)==3:
-        (h,w,d) = dst.shape
+        (h,w,d) = image.shape
     else:
-        (h,w),d = dst.shape,1
+        (h,w),d = image.shape,1
     hist_total = np.zeros([256,0], np.uint8)    
     for i in range(d):
         hist= cv2.calcHist([image], [i], None, [256], [0.0,255.0]) 
@@ -28,22 +28,18 @@ def get_cmu(hist):
 def get_match(dst,ref):
     (l,d) = dst.shape
     f_x = np.zeros(shape=dst.shape)
-
     for i in range(d):
         for j in range(l):
             dif_b = 1
             for k in range(l):
                 dif_a = abs(dst[j][i]-ref[k][i]) 
-                if(dif_a-dif_b<1.0e-08):
+                if(dif_a-dif_b<1.0e-8):
                     dif_b = dif_a
                     f_x[j][i] = k
-                else:
-                    f_x[j][i] = abs(k-1)
-                    break
-        if f_x[j][i]==255:
-            for k in range(j,l):
-                f_x[k][i]=255
-            break
+            if f_x[j][i]==255:
+                for p in range(j,l):
+                    f_x[p][i]=255
+                break
     return f_x
 
 def get_exp_match(dst,cu_dst):
@@ -53,7 +49,6 @@ def get_exp_match(dst,cu_dst):
         for j in range(l):
             lamda = 3*np.mean(dst)
             cu_exp[j][i] = exp_cdf(j,lamda)
-    print cu_dst.shape,cu_ref.shape
     f_x = get_match(cu_dst,cu_exp) 
     return f_x
 
@@ -61,13 +56,15 @@ def exp_cdf(x,lamda):
     return 1-m.exp(-lamda*x)
 
 if __name__ == '__main__':
-    ref_path = "./pic/output.png"
-    #dst_path = "./pic/LENA.jpg"
-    dst_path = "./pic/s.png"
-    out_path = "./pic/mix_out.png"
-    
-    ref = cv2.imread(ref_path,0)
-    dst = cv2.imread(dst_path,0)
+    #ref_path = "./pic/LENA.jpg"
+    #dst_path = "./pic/s.png"
+    #out_path = "./pic/exp_out.png"
+    dst_path = sys.argv[1]   
+    ref_path = sys.argv[2]   
+    out_path = sys.argv[3]   
+ 
+    ref = cv2.imread(ref_path)
+    dst = cv2.imread(dst_path)
 
     if len(dst.shape)==3:
         (h,w,d) = dst.shape
@@ -91,7 +88,7 @@ if __name__ == '__main__':
             for k in range(w):
                 pix = dstch[i][j][k]
                 dstch[i][j][k] = f_x[pix][i]
-    
+   
     merge = cv2.merge(dstch)
     #cv2.imshow("dst",dst)
     #cv2.imshow("ref",ref)
